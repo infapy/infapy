297f740ebdf504db1d7a66070476e5f30f17eaa3
Prashanth Pradeep <prashanth.pradeep96@gmail.com>
2021-09-19 18:16:25 +0530

    Hackatho01 - Push

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup (
     name="infapy",                     # This is the name of the package
-    version="1.0.1",                        # The initial release version
+    version="1.0.2",                        # The initial release version
     author="Prashanth",                     # Full name of the author
-    description="Quicksample test login",
+    description="Automation Tool for informatica Cloud",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     #packages=setuptools.find_packages(),    # List of all python modules to be installed
@@ -20,5 +20,5 @@ setuptools.setup (
     python_requires='>=3.7',                # Minimum version requirement of the package
     packages =["infapy"],             # Name of the python package
     # clspackage_dir={'infapy':''},     # Directory of the source code of the package
-    install_requires=["requests"]                     # Install other dependencies if any
+    install_requires=["requests","cryptography"]                     # Install other dependencies if any
 )
