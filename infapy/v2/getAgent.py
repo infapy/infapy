5e90a6fc6296cefd8076a348f866a89fa5146017
suvam7707 <suvam7707@gmail.com>
2021-09-23 18:28:07 +0530

    getAgentDetailsById and getAgentDetailsByName implemented

--- infapy/v2/getAgent.py
+++ infapy/v2/getAgent.py
@@ -62,6 +62,62 @@ class GetAgent:
         except Exception as e:
             infapy.log.exception(e)
             raise
-        infapy.log.info("Fetched Agent Details for All Agents in the Org")
+        infapy.log.info("Fetched Installer Link and Registration Token")
+        data = response.json()
+        return data
+
+    def getAgentDetailsById(self,id):
+        """getAgentDetailsById returns details for the Agent specified by the Id.
+
+        Args:
+            id (string): Agent Id.
+
+        Returns:
+            List of dict: <Agent Details in dict Format>
+        """
+    
+        url=self._v2BaseURL + "/api/v2/agent/" + id
+        headers = {'Content-Type': "application/json", 'Accept': "application/json","icSessionID":self._v2icSessionID}
+        infapy.log.info("getInstallerToken URL - " + url)
+        infapy.log.info("API Headers: " + str(headers))
+        infapy.log.info("Body: " + "This API requires no body")
+        # The below format is for post
+        # bodyV3={"username": userName,"password": password}
+        # r3 = re.post(url=urlV3, json=bodyV3, headers=headers)
+        try:
+            response = re.get(url=url, headers=headers)
+            infapy.log.debug(str(response.json()))
+        except Exception as e:
+            infapy.log.exception(e)
+            raise
+        infapy.log.info("Fetched Agent Details for Agent ID: " + id)
+        data = response.json()
+        return data
+
+    def getAgentDetailsByName(self,name):
+        """getAgentDetailsByName returns details for the Agent specified by the Name.
+
+        Args:
+            name (string): Agent Name.
+
+        Returns:
+            List of dict: <Agent Details in dict Format>
+        """
+    
+        url=self._v2BaseURL + "/api/v2/agent/name/" + name.replace(" ","%20")
+        headers = {'Content-Type': "application/json", 'Accept': "application/json","icSessionID":self._v2icSessionID}
+        infapy.log.info("getInstallerToken URL - " + url)
+        infapy.log.info("API Headers: " + str(headers))
+        infapy.log.info("Body: " + "This API requires no body")
+        # The below format is for post
+        # bodyV3={"username": userName,"password": password}
+        # r3 = re.post(url=urlV3, json=bodyV3, headers=headers)
+        try:
+            response = re.get(url=url, headers=headers)
+            infapy.log.debug(str(response.json()))
+        except Exception as e:
+            infapy.log.exception(e)
+            raise
+        infapy.log.info("Fetched Agent Details for Agent Name: " + name)
         data = response.json()
         return data
