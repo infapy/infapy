7be799ed6d2426a2a47cf1d458a3586bef7332d0
suvam7707 <suvam7707@gmail.com>
2021-09-25 17:18:57 +0530

    commit

--- infapy/v3/__init__.py
+++ infapy/v3/__init__.py
@@ -5,17 +5,17 @@ from infapy.exceptions import InvalidArgumentsError
 # infapy.log = logging.getinfapy.log(__name__)
 
 class V3():
-    """V3 class is a wrapper class for all the IICS rest v2 APIs
+    """V3 class is a wrapper class for all the IICS rest v3 APIs
     """
     def __init__(self,v3,v3BaseURL,v3SessionID):
-        """This class is used to leverage the IICS V2 APIs
+        """This class is used to leverage the IICS V3 APIs
 
         Args:
             v3 (json): login response of v23 login API
             v3BaseURL (string): The base url which we get from the v3 login API
             v3SessionID (string): The SessionID from the v3 Login API
         """
-        infapy.log.info("created a v2 handler object successfully")
+        infapy.log.info("created a v3 handler object successfully")
         infapy.log.info("v3BaseURL: " + v3BaseURL)
         self._v3=v3
         self._v3SessionID = v3SessionID
@@ -97,3 +97,23 @@ class V3():
     def userGroups(self):
         from infapy.v3.userGroups import UserGroups
         return UserGroups(self._v3, self._v3BaseURL, self._v3SessionID)
+
+    def schedule(self):
+        """This function returns an object of schedule
+        which has multiple sub methods to fetch, create, update and delete schedules in IICS
+
+        Returns:
+            class object: v3.schedule.Schedule
+        """
+        from infapy.v3.schedule import Schedule
+        return Schedule(self._v3,self._v3BaseURL,self._v3SessionID)
+
+    def license(self):
+        """This function returns an object of license
+        which has multiple sub methods to fetch and update licenses in IICS orgs
+
+        Returns:
+            class object: v3.license.License
+        """
+        from infapy.v3.license import License
+        return License(self._v3,self._v3BaseURL,self._v3SessionID)
